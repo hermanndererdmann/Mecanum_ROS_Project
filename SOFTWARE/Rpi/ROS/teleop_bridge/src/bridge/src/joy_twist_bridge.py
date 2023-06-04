08f51fa119432944905420ef6b7dae9d249157ff
florian <hermanndererdmann@gmail.com>
2023-06-04 16:26:41 +0200

    correction joy_twist_bridge & folder structure

--- SOFTWARE/Rpi/ROS/teleop_bridge/src/bridge/src/joy_twist_bridge.py
+++ SOFTWARE/Rpi/ROS/teleop_bridge/src/bridge/src/joy_twist_bridge.py
@@ -1,27 +1,12 @@
+#!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
-
-def joy_callback(data):
-    
-    # Erzeuge eine Instanz der Twist-Nachricht
-    twist_msg = Twist()
-    
-    # Weise Werte dem Vector3-Feld zu
-    twist_msg.linear.x = data.axes[0]
-    twist_msg.linear.y = data.axes[1]
-    twist_msg.linear.z = 0.0
-    
-    # Weise Werte dem angularen Vector3-Feld zu
-    twist_msg.angular.x = 0.0
-    twist_msg.angular.y = 0.0
-    twist_msg.angular.z = data.axes[4]
-    
-    # Veröffentliche die Twist-Nachricht
-    pub.publish(twist_msg)
+pub = None
 
 def joy_twist_bridge():
+    global pub
     rospy.init_node('joy_twist_bridge', anonymous=True)
 
     # Abonniere das Topic für die Joy-Nachricht
@@ -31,6 +16,26 @@ def joy_twist_bridge():
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
     rospy.spin()
+
+def joy_callback(data):
+    global pub
+    # Erzeuge eine Instanz der Twist-Nachricht
+    twist_msg = Twist()
+    
+    # Weise Werte dem Vector3-Feld zu
+    twist_msg.linear.x = data.axes[1]
+    twist_msg.linear.y = data.axes[0]
+    twist_msg.linear.z = 0.0
+    
+    # Weise Werte dem angularen Vector3-Feld zu
+    twist_msg.angular.x = 0.0
+    twist_msg.angular.y = 0.0
+    twist_msg.angular.z = data.axes[3]
+    
+    # Veröffentliche die Twist-Nachricht
+    pub.publish(twist_msg)
+
+
 
 if __name__ == '__main__':
     joy_twist_bridge()
